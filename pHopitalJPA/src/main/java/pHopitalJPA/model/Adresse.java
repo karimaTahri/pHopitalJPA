cb69402e422bf503669cffe0f60279f10cf01710
NEMMICHE <m.nemmiche93@gmail.com>
2018-03-02 13:01:14 +0100

    test

---
+++ pHopitalJPA/src/main/java/pHopitalJPA/model/Adresse.java
@@ -0,0 +1,66 @@
+package pHopitalJPA.model;
+
+import javax.persistence.Column;
+
+import com.fasterxml.jackson.annotation.JsonView;
+
+import formationJpa.model.view.JsonViews;
+
+public class Adresse {
+	@Column(name = "numero")
+	@JsonView(JsonViews.Common.class)
+	private Integer numero;
+	@Column(name = "rue", length = 200)
+	@JsonView(JsonViews.Common.class)
+	private String rue;
+	@Column(name = "code_postal", length = 5)
+	@JsonView(JsonViews.Common.class)
+	private String codePostal;
+	@Column(name = "ville", length = 150)
+	@JsonView(JsonViews.Common.class)
+	private String ville;
+
+	public Adresse() {
+
+	}
+
+	public Adresse(Integer numero, String rue, String codePostal, String ville) {
+		this.numero = numero;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+
+	public Integer getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Integer numero) {
+		this.numero = numero;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+}
