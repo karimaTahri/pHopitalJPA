c3a8de451885db1519a9399b4645cab27d245cd1
karimaTahri <karima.tahri@yahoo.fr>
2018-03-02 14:26:46 +0100

    kt

---
+++ pHopitalJPA/src/main/java/pHopitalJPA/repository/VisiteRepository.java
@@ -0,0 +1,11 @@
+package pHopitalJPA.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+import pHopitalJPA.model.Visite;
+
+
+public interface VisiteRepository extends JpaRepository<Visite, Integer> {
+
+}
